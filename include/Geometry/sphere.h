57e31b9a25967598d510a758bd06cc14ff537f8f
Jerry <Jerry_Shen0527@outlook.com>
2020-10-07 15:53:43 +0800

    sphere

--- include/Geometry/sphere.h
+++ include/Geometry/sphere.h
@@ -4,7 +4,6 @@
 #include <Geometry/hittable.h>
 #include <Tools/Math/onb.h>
 
-
 #include "Shape.h"
 #include "translation.h"
 
@@ -31,18 +30,31 @@ public:
 	std::shared_ptr<Material> mat_ptr;
 };
 
-class Sphere:public Shape
+class Sphere :public Shape
 {
 public:
-		Sphere(const Transform * ObjectToWorld, const Transform * WorldToObject,
-			bool reverseOrientation, Float radius, Float zMin, Float zMax,
-			Float phiMax)
-		: Shape(ObjectToWorld, WorldToObject, reverseOrientation),
-		radius(radius), zMin(Clamp(std::min(zMin, zMax), -radius, radius)),
-		zMax(Clamp(std::max(zMin, zMax), -radius, radius)),
-		thetaMin(std::acos(Clamp(zMin / radius, -1, 1))),
-		thetaMax(std::acos(Clamp(zMax / radius, -1, 1))),
-		phiMax(Radians(Clamp(phiMax, 0, 360))) { }
+	Sphere(const Transform* ObjectToWorld, const Transform* WorldToObject, bool reverseOrientation, Float radius, Float zMin, Float zMax, Float phiMax)
+		: Shape(ObjectToWorld, WorldToObject, reverseOrientation), radius(radius), zMin(Clamp(std::min(zMin, zMax), -radius, radius)), zMax(Clamp(std::max(zMin, zMax), -radius, radius)), thetaMin(std::acos(Clamp(zMin / radius, -1, 1))), thetaMax(std::acos(Clamp(zMax / radius, -1, 1))), phiMax(Radians(Clamp(phiMax, 0, 360))) { }
+
+	Bounds3f Sphere::ObjectBound() const {
+		return Bounds3f(Point3f(-radius, -radius, zMin),
+			Point3f(radius, radius, zMax));
+	}
+
+	bool Sphere::Intersect(const ray& r, Float& tHit, SurfaceInteraction& isect, bool testAlphaTexture) const {
+		Float phi;
+		Point3f pHit;
+		//Transform Ray to object space 134
+		//	Compute quadratic sphere coefficients 135
+		//	Solve quadratic equation for t values 136
+		//	Compute sphere hit position and ¦Õ 137
+		//	Test sphere intersection against clipping parameters 137
+		//	Find parametric representation of sphere hit 137
+		//	Compute error bounds for sphere intersection 225
+		//	Initialize SurfaceInteraction from parametric information 140
+		//	Update tHit for quadric intersection 140
+		return true;
+	}
 
 private:
 	const Float radius;
